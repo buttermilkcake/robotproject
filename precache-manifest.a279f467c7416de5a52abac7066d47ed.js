450b4740cdbee951f2c136ca4ded8b939bc7645f
Julie Stark <cakelp21@gmail.com>
2021-10-18 14:39:00 -0500

    Updates

---
+++ precache-manifest.a279f467c7416de5a52abac7066d47ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984bf2c12037d278f3c7b81d5a6f139a",
+    "url": "/robotproject/index.html"
+  },
+  {
+    "revision": "f96d92a1f24798bdc821",
+    "url": "/robotproject/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "8c75fb22e954f7854fd9",
+    "url": "/robotproject/static/css/main.6969304b.chunk.css"
+  },
+  {
+    "revision": "f96d92a1f24798bdc821",
+    "url": "/robotproject/static/js/2.27f60432.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robotproject/static/js/2.27f60432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c75fb22e954f7854fd9",
+    "url": "/robotproject/static/js/main.b892e5c5.chunk.js"
+  },
+  {
+    "revision": "fa5571c6014fb7f068d3",
+    "url": "/robotproject/static/js/runtime-main.5c10feb6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotproject/static/media/SEGA.12a3db8b.woff"
+  }
+]);
